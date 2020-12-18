7eaa9d1ccc845d57f51b1e6c5c70838be739a886
Mauro Lepore <maurolepore@gmail.com>
2020-12-18 10:43:31 -0300

    WIP remove fs

--- tests/testthat/helper-find_pacta.R
+++ tests/testthat/helper-find_pacta.R
@@ -1,4 +1,4 @@
 create_file_in_child <- function(parent, child, family = ".family") {
   child_path <- fs::dir_create(path(parent, child))
-  fs::file_create(path(child_path, family))
+  file.create(path(child_path, family))
 }
