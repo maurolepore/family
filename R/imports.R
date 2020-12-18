4d256abebd0407b24dc6f42720ba1defd1e8b66b
Mauro Lepore <maurolepore@gmail.com>
2020-12-18 11:11:13 -0300

    Avoid dependency on fs

--- R/imports.R
+++ R/imports.R
@@ -1,2 +1,21 @@
-#' @importFrom fs path path_dir path_file
-NULL
+# Avoid dependency on fs
+dir_create <- function(...) {
+  path <- path(...)
+  dir.create(path, recursive = TRUE)
+  invisible(path)
+}
+path <- function(...) file.path(...)
+file_create <- function(...) file.create(...)
+dir_ls <- function(...) list.files(...)
+
+path_file <- function (path) {
+  is_missing <- is.na(path)
+  path[!is_missing] <- basename(path[!is_missing])
+  as.character(path)
+}
+
+path_dir <- function (path) {
+  is_missing <- is.na(path)
+  path[!is_missing] <- dirname(path[!is_missing])
+  as.character(path)
+}
